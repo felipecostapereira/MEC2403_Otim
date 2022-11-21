849bbb26ed98761ea80c205aa0856c4d60f62653
Felipe Pereira <felipecostapereira@gmail.com>
2022-11-21 13:24:03 -0300

    t02 update

--- Trabalho_2/plot_phi_c.m
+++ Trabalho_2/plot_phi_c.m
@@ -1,4 +1,4 @@
-function [] = plot_phi(xmin, xmax, ymin, ymax, phi, intervals, c, x, graph_title, k, i, j)
+function [] = plot_phi_c(xmin, xmax, ymin, ymax, phi, intervals, c1, c2, x, graph_title, k, i, j)
 %     figure
     x1_ = linspace(xmin, xmax, 100);
     x2_ = linspace(ymin, ymax, 100);
@@ -11,7 +11,12 @@ function [] = plot_phi(xmin, xmax, ymin, ymax, phi, intervals, c, x, graph_title
         contour(x1, x2, phi(x1,x2), intervals);
     end
     hold on;
-    plot(x1_, c(x1_), 'r-', 'LineWidth', 2);  
+    contour(x1, x2, c1(x1,x2), [0 0], 'LineWidth', 3, 'LineColor', 'r');
+    hold on;
+    if ~isempty(c2)
+        contour(x1, x2, c2(x1,x2), [0 0], 'LineWidth', 3, 'LineColor', 'k');
+    end
+
     hold on;  
     for k = 1:length(x)
         plot(x(1,k), x(2,k),'o', 'LineWidth', 2, 'MarkerSize', 10)
@@ -19,8 +24,10 @@ function [] = plot_phi(xmin, xmax, ymin, ymax, phi, intervals, c, x, graph_title
             desenha_flecha(x(:,k)', x(:,k+1)', 'k');
         end
     end
-    title(graph_title)
+    sgtitle(graph_title)
     xlabel('$x_{1}$', 'Interpreter', 'latex')
+%     xlim([0 10])
+    set(gca, 'XScale', 'log')
     ylabel('$x_{2}$', 'Interpreter', 'latex')
 end
 % 
